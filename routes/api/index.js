efda390a192bff2bf70fb582744df224f932101d
Ken <kanney356@gmail.com>
2024-02-16 23:46:52 -0800

    fixed bugs and errors

--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const apiRoutes = require("./api-routes");
-
-router.use("/api", apiRoutes);
-router.use((req, res) => {
-  return res.status(404).send("Not Found");
-});
-
-module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const apiRoutes = require("./api-routes");
 
-const userRoutes = require("./user-routes");
-const thoughtRoutes = require("./thought-routes");
-
-router.use("/user", userRoutes);
-router.use("/thought", thoughtRoutes);
+router.use("/api", apiRoutes);
+router.use((req, res) => {
+  return res.status(404).send("Not Found");
+});
 
 module.exports = router;
+
